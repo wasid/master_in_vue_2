7fee53c265d8a5c2acaa1212ee997666901cb9ba
Wasid Hossain <wasidmail@gmail.com>
2017-06-02 10:04:39 +0000

    Mutating the Store

--- jokester/src/main.js
+++ jokester/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './components/App.vue'
 import store from './store'
 
+store.commit('INIT_JOKES', [{test: 'test jokes'}, {test2: 'test jokes 2'}])
+
 new Vue({
   el: '#app',
   store,
