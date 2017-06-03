bd034d31f3d075d9ce0681aeb10b404c3d092b02
Wasid Hossain <wasidmail@gmail.com>
2017-06-03 10:46:16 +0000

    Filtering Jokes

--- jokester/src/components/App.vue
+++ jokester/src/components/App.vue
@@ -5,10 +5,20 @@
     <button class="btn btn-success" @click="initJokes">Add Ten Random Jokes</button>
     <button class="btn btn-warning" @click="addJoke">Add One Random Joke</button>
     <br>
+    <span v-for="type in types">
+    <input
+      type="checkbox"
+      :value="type"
+      v-model="checkedTypes"
+      checked
+    >
+    <label>{{type}}</label>&nbsp;
+    </span>
     <br>
     <div class="col-md-12">
       <Joke
         v-for="(joke, index) in $store.state.jokes"
+        v-show="checkedTypes.includes(joke.type)"
         :joke="joke"
         :index="index"
         key="index"
@@ -23,7 +33,14 @@
 import { mapActions } from 'vuex'
 import Joke from './Joke.vue'
 
+
 export default {
+  data() {
+    return {
+      types: ['general', 'knock-knock', 'programming'],
+      checkedTypes: ['general', 'knock-knock', 'programming']
+    }
+  },
   methods: mapActions([
     'initJokes',
     'addJoke'
