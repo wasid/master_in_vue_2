be75126aed34dbbaa0379d59f293b5de1f9fed5e
Wasid Hossain <wasidmail@gmail.com>
2017-06-06 08:36:49 +0000

    Setting Up Turnout

---
+++ turnout/src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    App component
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: 'app',
+//   data () {
+//     return {
+//       msg: 'Welcome to Your Vue.js App'
+//     }
+//   }
+// }
+</script>
