02bc7fd37081bcdd1f929511b97313f9a3feaaad
Wasid Hossain <wasidmail@gmail.com>
2017-06-08 06:25:32 +0000

    google firebase added for route authentication

---
+++ turnout/src/firebaseApp.js
@@ -0,0 +1,12 @@
+  import firebase from 'firebase'
+  
+  const config = {
+    apiKey: "AIzaSyAoHGdmNfp9yVGnIz0phCIMaPN9WE6ujbY",
+    authDomain: "turnout-in-vue2.firebaseapp.com",
+    databaseURL: "https://turnout-in-vue2.firebaseio.com",
+    projectId: "turnout-in-vue2",
+    storageBucket: "turnout-in-vue2.appspot.com",
+    messagingSenderId: "710578293384"
+  };
+  
+  export const firebaseApp = firebase.initializeApp(config);
