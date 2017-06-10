ec6bc71cb92eb0fbabccc32a2e075b22183f6038
Wasid Hossain <wasidmail@gmail.com>
2017-06-10 06:25:27 +0000

    Submitting Events to firebase database with related user added

--- turnout/src/components/Dashboard.vue
+++ turnout/src/components/Dashboard.vue
@@ -1,19 +1,29 @@
 <template>
-  <div id="">
-  <h3>Events Dashboard</h3>
-    <button class="btn btn-danger btn-sm" @click="signOut">Sign Out</button>
-  <hr>
-    {{ $store.state }}
+  <div class="col-sm-12">
+      <h3>Events Dashboard</h3>
+      <button class="btn btn-danger btn-sm" @click="signOut">Sign Out</button>
+      <br>
+      <br>
+      <AddEvent />
+      <br>
+      <br>
+      <div class="col-sm-12">
+        {{ $store.state }}
+      </div>
   </div>
 </template>
 <script>
-  import { firebaseApp } from '../firebaseApp'
+  import { firebaseApp, eventsRef } from '../firebaseApp'
+  import AddEvent from './AddEvent.vue'
   export default {
   methods: {
     signOut() {
       this.$store.dispatch('signOutRush')
       firebaseApp.auth().signOut()
     }
+  },
+  components: {
+    AddEvent
   }
 }
 </script>
